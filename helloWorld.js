8981d99d8415f2e5b9fef6163484f36e65a88d1c
Angel Santana <angel.d.santana00@gmail.com>
2021-11-30 19:57:56 -0600

    add the first test and solution for intro-to-testing

--- helloWorld.js
+++ helloWorld.js
@@ -3,6 +3,10 @@ function helloWorld(input) {
     return "Hello, World!";
 }
 
-function sayHello () {
-
+function sayHello(name) {
+    if (name === undefined || name === true || name === false) {
+        return "Hello, World!";
+    } else {
+        return "Hello, " + name + "!";
+    }
 }
